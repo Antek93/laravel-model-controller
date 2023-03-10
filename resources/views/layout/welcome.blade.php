0a6b7f590548861dbf5b14c4bd17255f01585dd5
Antek93 <antonio.savino1993@gmail.com>
2023-03-10 17:34:45 +0100

    ese completato

--- resources/views/layout/welcome.blade.php
+++ resources/views/layout/welcome.blade.php
@@ -1,33 +1,51 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Model Controller</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Includiamo gli assets con la direttiva @vite --}}
-        @vite('resources/js/app.js')
-    </head>
-    <body>
+    <title>Laravel Model Controller</title>
 
-        <main>
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center">
+    {{-- Includiamo gli assets con la direttiva @vite --}}
+    @vite('resources/js/app.js')
+</head>
 
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
+<body>
 
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex justify-content-between">
+                            @foreach($movies as $movie)
+                            <div>
+                                <div class="movieContainer">
+                                    {{ $movie['title'] }}
+                                </div>
+                                <div class="movieContainer">
+                                    {{ $movie['original_title'] }}
+                                </div>
+                                <div class="movieContainer">
+                                    {{ $movie['nationality'] }}
+                                </div>
+                                <div class="movieContainer">
+                                    {{ $movie['date'] }}
+                                </div>
+                                <div class="movieContainer">
+                                    {{ $movie['vote'] }}
+                                </div>
                             </div>
-                        </div>
-
+                        
+    
+                            @endforeach
+                        </ul>
                     </div>
                 </div>
             </div>
-        </main>
+        </div>
+    </main>
 
-    </body>
+</body>
+
 </html>
